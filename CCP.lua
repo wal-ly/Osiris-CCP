5588901fc9b711dffe4172dd52f491125e39665d
walle <100832774+wal-ly@users.noreply.github.com>
2024-04-08 17:07:59 +0500

    Update CCP.lua

--- CCP.lua
+++ CCP.lua
@@ -1,15 +1,6 @@
--- Create a table to store your settings.
-print("Script initiated")
-local settings = {}
+-- Create a table to hold functions with Unicode keys
+local functions = {}
+functions["打印"] = print
 
--- Assign the "enabled" property to the Chinese word "启用" within the table.
-settings["启用"] = true
-print("Table made")
--- Function to print the value of "enabled" in Chinese.
-local function 打印启用()
-    print(settings["启用"])
-end
-print("Function Made")
--- Call the function to print the value.
-打印启用()  -- Output should be: true
-print("function output successful")
+-- You can then access the function using the Unicode string
+functions["打印"]("Hello world")
