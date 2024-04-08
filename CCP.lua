fd29f6464473746a74a0a76271eaa7bda7d1bd0c
walle <100832774+wal-ly@users.noreply.github.com>
2024-04-08 17:12:11 +0500

    Update CCP.lua

--- CCP.lua
+++ CCP.lua
@@ -1,6 +1,6 @@
 -- Create a table to hold functions with Unicode keys
 local functions = {}
-functions["rawr"] = print
+functions["打印"] = print
 
 -- You can then access the function using the Unicode string
-functions["rawr"]("Hello world")
+functions["打印"]("Hello world")
